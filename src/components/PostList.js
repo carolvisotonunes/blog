1ba40d9f67765b3aba1540fab7592ed0057b62c5
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-06-30 19:53:03 +1000

    syntaxe with redux tunk

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,9 +4,15 @@ import {fetchPosts} from '../actions';
 
 
 class PostList extends React.Component {
+componentDidMount(){
+ this.props.fetchPosts();
+}
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+export default  connect (
+null,
+{fetchPosts}
+)(PostList);
