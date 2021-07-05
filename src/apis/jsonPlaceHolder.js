65b8710409519ff2a13e9940aa9e84d452404d29
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-07-05 17:40:31 +1000

    dispatching correct values

--- src/apis/jsonPlaceHolder.js
+++ src/apis/jsonPlaceHolder.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 
 export default axios.create({
-  baseURl: 'https://jsonplaceholder.typicode.com'
+  baseURL: 'https://jsonplaceholder.typicode.com'
 });
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,11 +8,15 @@ componentDidMount(){
  this.props.fetchPosts();
 }
   render() {
+   console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
+const mapStateToProps =  state => {
+return {posts: state.posts};
+};
 
 export default  connect (
-null,
+mapStateToProps,
 {fetchPosts}
 )(PostList);
